34c8bbc991a74092ffc933c9aa59819d179df72f
MyungJin Eun <eunmyun@uw.edu>
2016-11-22 05:23:55 -0800

    asldkjf

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -14,7 +14,7 @@ var ProjectPage = React.createClass({
 
     componentDidMount() {
       console.log("componentDidMount");
-      $.get('data/project.json').then(function(data) {
+      $.get('../data/project.json').then(function(data) {
         console.log("data.projects is " + data.projects);
         this.setState({projectArray:data.projects})
       }.bind(this));
